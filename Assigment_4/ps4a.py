c4912d37ff6d15b95e43cb930b1546bd0cb105bc
root <root@justdial.(none)>
2017-02-20 13:29:05 +0530

    Finished part 1 of Word Game

--- Assigment_4/ps4a.py
+++ Assigment_4/ps4a.py
@@ -71,8 +71,16 @@ def getWordScore(word, n):
     n: integer (HAND_SIZE; i.e., hand size required for additional points)
     returns: int >= 0
     """
-    # TO DO ... <-- Remove this comment when you code this function
-
+    total = 0
+    length = len(word)
+    if length == 0:
+        return 0
+    for letter in word:
+        total += SCRABBLE_LETTER_VALUES[letter]
+    total *= length
+    if length >= n:
+        total += 50
+    return total
 
 
 #

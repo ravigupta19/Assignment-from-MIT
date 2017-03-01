443308d6376b01ba5158a24460b939a382aa76ab
root <root@justdial.(none)>
2017-03-01 11:58:00 +0530

    Mid Term Assigment Complete code of Diff_Intersect

--- Midterm_assignment/dict_interdiff.py
+++ Midterm_assignment/dict_interdiff.py
@@ -1,13 +1,13 @@
 d1 = {1:30, 2:20, 3:30, 5:80,7:20}
 d2 = {1:40, 2:50, 3:60, 4:70, 6:90,7:90}
 
-def add(a, b):
+def f(a, b):
     return a + b
 
 def dict_interdiff(d1, d2):
-    return_tuple = ()
     dict_intersect = {}
-    dict_diff  = {}
+    dict_diff = {}
+    return_tuple = (dict_intersect, dict_diff)
     diff_keys = (d1.keys() - d2.keys() | d2.keys() - d1.keys())
     if len(d1) > len(d2):
         iterate = d1
@@ -15,6 +15,15 @@ def dict_interdiff(d1, d2):
         iterate = d2
     for keys in iterate:
         if keys not in diff_keys:
-            a
+            a = d1[keys]
+            b = d2[keys]
+            res = f(a, b)
+            dict_intersect[keys] = res
 
-dict_interdiff(d1,d2)
+    for key in diff_keys:
+        if key in d1:
+            dict_diff[key] = d1[key]
+        else:
+            dict_diff[key] = d2[key]
+    return return_tuple
+print(dict_interdiff(d1,d2))

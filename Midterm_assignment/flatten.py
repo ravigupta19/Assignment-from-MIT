69bc98b8da25bff9770e2561cf5e52ba10349563
root <root@justdial.(none)>
2017-03-09 17:07:01 +0530

    Started owkring on Assigment 5

--- Midterm_assignment/flatten.py
+++ Midterm_assignment/flatten.py
@@ -1,1 +1,18 @@
+def flatten(aList):
+    '''
+    aList: a list
+    Returns a copy of aList, which is a flattened version of aList
+    '''
+    List = []
+    for i in aList:
+        if isinstance(i,list):
+            List.extend(flatten(i))
+        else:
+            List.append(i)
+    return List
+
+#List = []
+aList = [[1, 'a', ['cat'], 2], [[[3]], 'dog'], 4, 5]
+
+print(flatten(aList))
 

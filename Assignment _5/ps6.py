ac50838fd0cf73879e1d0f5141c097da2d2530eb
root <root@justdial.(none)>
2017-03-09 18:11:14 +0530

    Finished Shift_Dict  of Part1

--- Assignment _5/ps6.py
+++ Assignment _5/ps6.py
@@ -94,15 +94,23 @@ class Message(object):
         The dictionary maps every uppercase and lowercase letter to a
         character shifted down the alphabet by the input shift. The dictionary
         should have 52 keys of all the uppercase letters and all the lowercase
-        letters only.        
-        
-        shift (integer): the amount by which to shift every letter of the 
+        letters only.
+
+        shift (integer): the amount by which to shift every letter of the
         alphabet. 0 <= shift < 26
 
-        Returns: a dictionary mapping a letter (string) to 
-                 another letter (string). 
+        Returns: a dictionary mapping a letter (string) to
+                 another letter (string).
         '''
-        pass #delete this line and replace with your code here
+        shift_dict = {}
+        alphabets = string.ascii_lowercase
+
+        for letter in alphabets:
+            indexAlphabet = (alphabets.index(letter) + shift) % 26
+            shift_dict[letter] = alphabets[indexAlphabet]
+            shift_dict[letter.upper()] = alphabets[indexAlphabet].upper()
+        return shift_dict
+
 
     def apply_shift(self, shift):
         '''
@@ -116,7 +124,8 @@ class Message(object):
         Returns: the message text (string) in which every character is shifted
              down the alphabet by the input shift
         '''
-        pass #delete this line and replace with your code here
+        pass
+
 
 class PlaintextMessage(Message):
     def __init__(self, text, shift):
